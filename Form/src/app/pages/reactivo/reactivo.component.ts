67fa11f2e214cac3c23449dc6bb7c50b10f5e906
adan.tarazona <adangabriel1@gmail.com>
2023-05-15 09:55:11 -0300

    form reactivo && template v2

--- Form/src/app/pages/reactivo/reactivo.component.ts
+++ Form/src/app/pages/reactivo/reactivo.component.ts
@@ -7,26 +7,48 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./reactivo.component.scss']
 })
 export class ReactivoComponent implements OnInit {
-  form:FormGroup;
+  form: FormGroup;
 
-  constructor(){}
+  constructor() { }
   ngOnInit(): void {
     this.createForm();
+    this.listenChanges();
+
+
+
+
   }
 
-  save(){
+  save() {
     console.log(this.form);
     console.log(this.form.value)
     console.table(this.form.value)
   }
 
-  createForm(){
+  listenChanges() {
+    this.form.valueChanges.subscribe((value) => {
+      console.log(value.edad)
+      if (value.edad >= 18 && !this.form.controls['drink'].enabled) {
+        this.form.controls['drink'].enable();
+        this.form.controls['drink'].setValidators([Validators.required]);
+        this.form.controls['drink'].updateValueAndValidity();
+      } else if (value.edad < 18 && this.form.controls['drink'].enabled) {
+        this.form.controls['drink'].disable();
+        this.form.controls['drink'].removeValidators([Validators.required]);
+        this.form.controls['drink'].updateValueAndValidity();
+      }
+    })
+  }
+
+  createForm() {
     this.form = new FormGroup({
       nombre: new FormControl<string>('', [Validators.minLength(2), Validators.required]),
       apellido: new FormControl('', [Validators.minLength(3), Validators.required]),
       email: new FormControl('', [Validators.email, Validators.required]),
-      edad : new FormControl<number>(null, [Validators.min(0), Validators.max(99), Validators.required])
+      edad: new FormControl<number>(null, [Validators.min(0), Validators.max(99), Validators.required]),
+      drink: new FormControl<string>('', [Validators.minLength(2)])
     })
+    this.form.controls['drink'].disable();
   }
 
 
--- Form/src/app/pages/template/template.component.ts
+++ Form/src/app/pages/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class TemplateComponent {
 
+  user: User= new User();
+  disableDrink : boolean = true;
+
+  save(form : NgForm) {
+    console.log(form.form)
+  }
+
+  changeAge(form: NgForm){
+    if(this.user.edad >=18 && this.disableDrink){
+        this.disableDrink = false;
+        form.controls['drink'].setValidators([Validators.required])
+        form.controls['drink'].updateValueAndValidity();
+    } else  if(this.user.edad < 18 && !this.disableDrink) {
+        this.disableDrink = true;
+        form.controls['drink'].removeValidators([Validators.required]);
+        form.controls['drink'].updateValueAndValidity();
+    }
+  }
 }
+
+
+class User {
+  nombre : String;
+  apellido : String;
+  email: String;
+  edad: number;
+  drink: string;
+}
+
